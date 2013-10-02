fca70c69cdfa404d99c353b5004b1cb72c08a336
Sebastien MICHEL <michoux@gmail.com>
2013-10-02 22:02:53 +0200

    Add unit tests
    Add unit tests for tgid and tracing_mark_write plugin
    
    Signed-off-by: Sebastien MICHEL <michoux@gmail.com>

--- timechart/tests/test_ftrace.py
+++ timechart/tests/test_ftrace.py
@@ -8,3 +8,11 @@ class TestFtrace(BackendTestCase):
     def test_rpm(self):
         self.load_file("rpm-trace.txt.gz", "ftrace")
         self.assertHasProcess("rpm_cb:usb1:0 (76.0 us)")
+    def test_tgidtrace(self):
+        proj = self.load_file("tgidtrace.txt", "ftrace")
+        self.assertHasProcess("er.ServerThread:457 (23.6 ms)")
+    def test_tracing_mark_write(self):
+        proj = self.load_file("tgidtrace.txt", "ftrace")
+        self.assertHasProcess("id.defcontainer|sync|000:3611 (7.0 ms)")
+        self.assertHasProcess("er.ServerThread|sync|000:457 (64.9 ms)")
+        self.assertHasProcess("er.ServerThread|sync|001:457 (35.7 ms)")
